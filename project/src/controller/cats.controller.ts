17a875e7837e331e770db641bea2373124f21580
Vizcaino Perez <MRVIPEDA@intranet.ibermatica>
2018-08-21 12:43:16 +0200

    second commit

--- project/src/controller/cats.controller.ts
+++ project/src/controller/cats.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get} from '@nestjs/common';
+import { Controller, Get, Req, Post, HttpCode} from '@nestjs/common';
+import { create } from 'domain';
 
 @Controller('cats')
 export class CatsController {
-    @Get()
+    @Get('/findAll')
     finAll(){
         return 'This action returns all cats';
+    }
+    @Get('/showRequest')
+    findAll(@Req() req){
+        return req.body;
+    }
+
+    @Post('/addCats')
+    create(){
+        return 'Esta accion añade un nuevo gato';
+    }
+
+    @Post('/addCats2')
+    @HttpCode(204)
+    cahngeCodeResponse(){
+        return 'Esta accion añade un nuevo gato con codigo 204';
     }
 }
